a63b1b119e038120f596968aba9706294f672bdb
Sungjin Han <meinside@st-company.net>
2015-07-29 10:54:59 +0900

    add map file for debugging

--- bin/fetch-latest.rb
+++ bin/fetch-latest.rb
@@ -5,17 +5,19 @@
 # 
 # fetch latest phaser build from github
 # 
-# created on : 2014.04.30
-# last update: 2014.04.30
+# created on : 2014.04.30.
+# last update: 2015.07.29.
 # 
 # by meinside@gmail.com
 
 JS_URL     = "https://github.com/photonstorm/phaser/raw/master/build/phaser.js"
 JS_MIN_URL = "https://github.com/photonstorm/phaser/raw/master/build/phaser.min.js"
+JS_MAP_URL = "https://github.com/photonstorm/phaser/raw/master/build/phaser.map"
+
 JS_DIR     = File.expand_path(File.join(File.dirname(__FILE__), '..', 'vendor', 'assets', 'javascripts'))
 
 if __FILE__ == $0
-  [JS_URL, JS_MIN_URL].each{|url|
+  [JS_URL, JS_MIN_URL, JS_MAP_URL].each{|url|
     filepath = File.join(JS_DIR, File.basename(url))
     `wget -O '#{filepath}' '#{url}'`
   }
